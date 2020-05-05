7992d0c43a9022fa942e10ed3168c00bb310fc8c
Ignacio Acisclo Pérez <iacisclo@autentia.com>
2020-05-05 07:27:59 +0200

    Initial Commit

---
+++ ViperNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViperNotes
+//
+//  Created by Ignacio Acisclo on 05/05/2020.
+//  Copyright © 2020 Ignacio Acisclo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViperNotesTests/ViperNotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViperNotesTests.swift
+//  ViperNotesTests
+//
+//  Created by Ignacio Acisclo on 05/05/2020.
+//  Copyright © 2020 Ignacio Acisclo. All rights reserved.
+//
+
+import XCTest
+@testable import ViperNotes
+
+class ViperNotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
